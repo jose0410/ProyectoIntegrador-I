f564e48d80f19025819c1a043c5f53386ea0fa01
Alejandro Granada Restrepo <alejandro.granada@talosdigital.com>
2017-10-04 00:24:03 -0500

    add navbar component

--- src/main/webapp/app/app.route.ts
+++ src/main/webapp/app/app.route.ts
@@ -1,9 +1,9 @@
 import { Route } from '@angular/router';
 
-import { NavbarComponent } from './layouts';
+import { PiNavbarComponent } from './layouts';
 
 export const navbarRoute: Route = {
     path: '',
-    component: NavbarComponent,
-    outlet: 'navbar'
+    component: PiNavbarComponent,
+    outlet: 'jhi-pi-navbar'
 };
