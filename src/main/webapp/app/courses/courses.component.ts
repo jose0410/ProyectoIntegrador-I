9f563b715b2791ee6005d11f132d7d1a2d1fe5c1
Alejandro Granada Restrepo <alejandro.granada@talosdigital.com>
2017-10-04 23:39:32 -0500

    Add courses component html and ts

---
+++ src/main/webapp/app/courses/courses.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'jhi-pi-courses',
+    templateUrl: './courses.component.html',
+    styles: []
+})
+export class CoursesComponent implements OnInit {
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+}
